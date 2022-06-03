76ef12c1cdbae2408ac464b7ffdf9cc5ec2f969c
Kai Kamf <74895715+Kai9073@users.noreply.github.com>
2022-06-03 18:45:34 +0800

    PYCNext Pre-alpha 0.4.3 - Mobile UI Enhancement

--- assets/js/pycnext/base.js
+++ assets/js/pycnext/base.js
@@ -4,12 +4,23 @@ $("[data-sidebar-collapse]").click(function (e) {
   $("#site-main-ico").attr("menu-toggler", "");
 });
 
-if (window.matchMedia("(max-width: 748px)").matches) {
-  $("#site-main-ico").attr("menu-toggler", "");
-  // add active attr to sec-sidebar when click on menu-toggler
-  $("#site-main-ico").click(function (e) {
-    e.preventDefault();
+function toggleSidebarIfMediaMatch() {
+  if (window.matchMedia("(max-width: 748px)").matches) {
+    $("#site-main-ico").attr("menu-toggler", "");
+    // add active attr to sec-sidebar when click on menu-toggler
+    $("#site-main-ico").click(function (e) {
+      e.preventDefault();
+      $("#sec-sidebar").attr("active", "");
+      $("#site-main-ico").removeAttr("menu-toggler");
+    });
+  } else {
     $("#sec-sidebar").attr("active", "");
     $("#site-main-ico").removeAttr("menu-toggler");
-  });
+  }
 }
+
+toggleSidebarIfMediaMatch();
+
+$(window).resize(function () {
+  toggleSidebarIfMediaMatch();
+});
